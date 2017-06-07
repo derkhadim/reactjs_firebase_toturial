7fc9c4a851e32dfd64c26b602c3a4658d5947464
Mouhamadou DER <dermouhamadou@gmail.com>
2017-06-07 11:28:58 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-    piKey: "AIzaSyD410mfMtdheCXBkk3X8sjB5ne2C119OmI",
+    piKey: "yusetrzy",
     authDomain: "react-firebase-food.firebaseapp.com",
     databaseURL: "https://react-firebase-food.firebaseio.com",
     projectId: "react-firebase-food",
     storageBucket: "react-firebase-food.appspot.com",
-    messagingSenderId: "708557153885"
+    messagingSenderId: "uyebgrec"
 };
 
 firebase.initializeApp(config);
